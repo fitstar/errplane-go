d03993219816f90274744bfdfd7a39db9078f870
Dave Grijalva <grijalva@gmail.com>
2013-04-02 16:38:53 -0700

    submit pipeline stages as context

--- falcore/falcore.go
+++ falcore/falcore.go
@@ -17,15 +17,28 @@ func NewTracer(ep *errplane.Client) *Tracer {
 	return &Tracer{ep: ep}
 }
 
+type traceContextJSON struct {
+	Stages []string `json:"stages"`
+}
+
 func (t *Tracer) Trace(req *falcore.Request, res *http.Response) {
 	e := new(errplane.Event)
 	e.Name = fmt.Sprintf("controllers/%v", req.Signature())
 	e.Value = float64(req.EndTime.Sub(req.StartTime)) / float64(time.Millisecond)
+
+	var context = new(traceContextJSON)
+	context.Stages = make([]string, req.PipelineStageStats.Len())
+	i := 0
+	for e := req.PipelineStageStats.Front(); e != nil; e = e.Next() {
+		pss, _ := e.Value.(*falcore.PipelineStageStat)
+		context.Stages[i] = fmt.Sprintf("%v:%v", pss.Name, pss.Status)
+		i++
+	}
+	e.Context = context
+	
 	if t.ep != nil {
 		t.ep.EnqueueEvent(e)
 	} else {
 		errplane.EnqueueEvent(e)
 	}
-
-	//TODO: more detail/break down by stage
 }
